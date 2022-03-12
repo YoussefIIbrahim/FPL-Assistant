9cb6a76751fb58f3fd36db975ac1646f0f1a5d28
Youssef Ibrahim <youssef.i.ibrahim@icloud.com>
2022-03-12 20:48:47 +0100

    Initial commit

---
+++ src/main/java/com/fpl/assistant/exhaust/FplAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.fpl.assistant.exhaust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FplAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FplAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fpl/assistant/exhaust/FplAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpl.assistant.exhaust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FplAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
